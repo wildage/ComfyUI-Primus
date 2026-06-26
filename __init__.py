55d938a19b469e368aaaa9f0c8916f8cdb4d623c
Primus <wildage@qq.com>
2026-06-27 02:01:39 +0800

    2.2.0-重组目录结构

--- __init__.py
+++ __init__.py
@@ -2,18 +2,7 @@
 ComfyUI-Primus - ComfyUI 自定义节点合集
 """
 
-# 导入服务端路由模块以注册 API 路由
-from . import selector_server  # noqa: F401
-
-from .customizable_selector import CustomizableSelector
-
-NODE_CLASS_MAPPINGS = {
-    CustomizableSelector.NAME: CustomizableSelector,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    CustomizableSelector.NAME: "Customizable Selector",
-}
+from .CustomizableSelector import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 # ComfyUI 自动加载此目录下的 JS 文件
 WEB_DIRECTORY = "./web"
